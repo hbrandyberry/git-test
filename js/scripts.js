98c6da2b539c0d31072c8f9ba3a488ed95057e8f
Hannah Brandyberry <hanniebrandyberry@gmail.com>
2022-10-08 12:07:03 -0400

    Week 4 Assignment

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(function() {
+    $(".carousel").carousel( { interval: 2000, pause: "false" } );
+    $("#carouselPause").click(function(){
+        $(".carousel").carousel("pause");
+    });
+    $("#carouselPlay").click(function(){
+        $(".carousel").carousel("cycle");
+    });
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal("show");
+    });
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("show");
+    });
+});
+
